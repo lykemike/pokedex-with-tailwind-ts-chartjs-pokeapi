4629fdacde6fd7695aee5f53b9cd9cae40fbeaa2
Michael Kevin Montolalu <61860743+lykemike@users.noreply.github.com>
2022-07-19 05:27:20 +0700

    Fix UI

--- components/Navigationbar.tsx
+++ components/Navigationbar.tsx
@@ -23,12 +23,29 @@ const Layout = ({ children, title }: any) => {
 
       <main className="container w-full min-h-screen p-2 mx-auto">{children}</main>
 
-      <div className="mt-auto">
-        <footer className="flex items-center justify-center mt-10 ">
-          Copyright by &copy;MKM
-          {/* <a className="flex flex-col" target="_blank" rel="noopener noreferrer">
-          Powered by <Image src="/domthedev.png" alt="Dom the dev Logo" width={72} height={72} />
-        </a> */}
+      <div className="p-4 mt-20 bg-gray-500">
+        <footer className="flex items-center justify-center space-x-2 tracking-wider">
+          {/* <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐝</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐜</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐌</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐙</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🦦</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐓</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐡</span> */}
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🦩</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🦆</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🦅</span>
+          <div className="font-mono text-xl font-semibold">MADE BY &copy; MKM</div>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🦈</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐟</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐦</span>
+          {/* <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐤</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🦎</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🦉</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐠</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🦢</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🕊️</span>
+          <span className="font-mono text-2xl font-semibold duration-500 hover:scale-125">🐧</span> */}
         </footer>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {
-      satoshi: "Satoshi, sans-serif",
-      inter: "Inter, sans-serif",
+    extend: {
+      fontFamily: {
+        satoshi: "Satoshi, sans-serif",
+        inter: "Inter, sans-serif",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
